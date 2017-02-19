639b6b981644abb95625dc43001d7b9eaa10a4c7
irinaleibutina <irinaleibutina@gmail.com>
2017-02-20 00:14:04 +0300

    Add registration

--- src/controller/command/impl/SignUp.java
+++ src/controller/command/impl/SignUp.java
@@ -3,14 +3,23 @@ package controller.command.impl;
 import controller.command.Command;
 import controller.command.exception.CommandException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by irinaleibutina on 19.02.17.
  */
 public class SignUp implements Command {
     @Override
-    public String execute(HttpServletRequest request) throws CommandException {
+    public String execute(HttpServletRequest request, HttpServletResponse response)
+            throws CommandException, ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/signUp.jsp");
+        requestDispatcher.forward(request, response);
         return null;
     }
 }
+
+
